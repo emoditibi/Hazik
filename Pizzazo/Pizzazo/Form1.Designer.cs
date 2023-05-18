a194bf457c0f6419bbdde0e04be0598e8324e6ab
TibiAHegyről <99481995+emoditibi@users.noreply.github.com>
2023-05-18 06:36:57 +0200

    Pizazzo -23.05.17

---
+++ Pizzazo/Pizzazo/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Pizzazo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            Feltétek = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            checkBox8 = new CheckBox();
+            checkBox7 = new CheckBox();
+            label1 = new Label();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.Location = new Point(14, 49);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(68, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Kolbász";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox2.Location = new Point(14, 87);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(47, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Sajt";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox3.Location = new Point(14, 126);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(83, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Pepperoni";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Feltétek
+            // 
+            Feltétek.AutoSize = true;
+            Feltétek.BackColor = Color.Maroon;
+            Feltétek.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            Feltétek.Location = new Point(14, 16);
+            Feltétek.Name = "Feltétek";
+            Feltétek.Size = new Size(110, 20);
+            Feltétek.TabIndex = 3;
+            Feltétek.Text = "Plusz Feltétek:";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Maroon;
+            panel1.BackgroundImageLayout = ImageLayout.Center;
+            panel1.Controls.Add(Feltétek);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(checkBox3);
+            panel1.Controls.Add(checkBox2);
+            panel1.Location = new Point(269, 77);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 207);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Maroon;
+            panel2.BackgroundImageLayout = ImageLayout.Center;
+            panel2.Controls.Add(checkBox8);
+            panel2.Controls.Add(checkBox7);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(checkBox4);
+            panel2.Controls.Add(checkBox5);
+            panel2.Controls.Add(checkBox6);
+            panel2.Location = new Point(826, 77);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(300, 244);
+            panel2.TabIndex = 5;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox8.Location = new Point(14, 204);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(83, 19);
+            checkBox8.TabIndex = 5;
+            checkBox8.Text = "Pepperoni";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox7.Location = new Point(14, 167);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(83, 19);
+            checkBox7.TabIndex = 4;
+            checkBox7.Text = "Pepperoni";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(14, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Pizzák:";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox4.Location = new Point(14, 49);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(68, 19);
+            checkBox4.TabIndex = 0;
+            checkBox4.Text = "Kolbász";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox5.Location = new Point(14, 126);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(83, 19);
+            checkBox5.TabIndex = 2;
+            checkBox5.Text = "Pepperoni";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox6.Location = new Point(14, 87);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(47, 19);
+            checkBox6.TabIndex = 1;
+            checkBox6.Text = "Sajt";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(615, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(161, 28);
+            label2.TabIndex = 6;
+            label2.Text = "Tibi  Papa Pizza";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.Maroon;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1420, 685);
+            Controls.Add(label2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.White;
+            Name = "Form1";
+            Text = "Program";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Label Feltétek;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox8;
+        private CheckBox checkBox7;
+        private Label label2;
+    }
+}
---
+++ Pizzazo/Pizzazo/Form1.cs
@@ -0,0 +1,12 @@
+namespace Pizzazo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
